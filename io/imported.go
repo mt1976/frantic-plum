92520b65a606356658f8aa72e742d5f9af2611f0
Matt Townsend <mt76@gmx.com>
2025-01-28 13:10:45 +0000

    Add base64 encoding/decoding and directory management functions

---
+++ io/imported.go
@@ -0,0 +1,41 @@
+package io
+
+import (
+	"encoding/base64"
+	"os"
+	"strings"
+
+	"github.com/mt1976/frantic-plum/logger"
+)
+
+func base64Encode(str string) string {
+	return base64.StdEncoding.EncodeToString([]byte(str))
+}
+
+func base64Decode(str string) (string, bool) {
+	data, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return "", true
+	}
+	return string(data), false
+}
+
+func MkdirAll(path string) error {
+	logger.InfoLogger.Printf("[%v] Creating folder Path=[%v]", strings.ToUpper(name), path)
+	return os.MkdirAll(path, os.ModeSticky|os.ModePerm)
+}
+
+func GetFolders(path string) ([]string, error) {
+	// Get all folders in the backup directory
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+	var folders []string
+	for _, file := range files {
+		if file.IsDir() {
+			folders = append(folders, file.Name())
+		}
+	}
+	return folders, nil
+}
