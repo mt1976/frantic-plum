7c2fa2ea4487b96046772bd5b4acc3703bdf3b60
Matt Townsend <mt76@gmx.com>
2025-02-24 14:11:46 +0000

    Enhance import/export functionality with improved logging and timing metrics

--- importExportHelper/importExportHelper.go
+++ importExportHelper/importExportHelper.go
@@ -10,8 +10,10 @@ import (
 	"time"
 
 	"github.com/gocarina/gocsv"
+	"github.com/mt1976/frantic-core/dao/actions"
 	"github.com/mt1976/frantic-core/logHandler"
 	"github.com/mt1976/frantic-core/paths"
+	"github.com/mt1976/frantic-core/timing"
 )
 
 // TemplateImportData is a struct to hold the data from the CSV file
@@ -20,12 +22,12 @@ import (
 // this struct should be customised to suit the specific requirements of the entryination table/DAO.
 
 var FIELDSEPARATOR = '|'
-var domain = "impex"
+var domain = "Import/Export"
 var importString = "Import"
 var exportString = "Export"
 
 func ExportCSV[T any](exportName string, exportList []T) error {
-	logHandler.ExportLogger.Printf("Exporting %v", exportName)
+	clock := timing.Start(exportName, actions.EXPORT.GetCode(), "")
 
 	exportFile := openTargetFile(exportName, exportString, logHandler.ExportLogger)
 	defer exportFile.Close()
@@ -52,7 +54,9 @@ func ExportCSV[T any](exportName string, exportList []T) error {
 
 	exportFile.Close()
 
-	logHandler.ExportLogger.Printf("Exported (%v) %vs", len(exportList), exportName)
+	logHandler.ExportLogger.Printf("Exported (%v/%v) %v(s) to [%v]", len(exportList), len(exportList), exportName, exportFile.Name())
+	logHandler.EventLogger.Printf("Exported (%v/%v) %v(s) to [%v]", len(exportList), len(exportList), exportName, exportFile.Name())
+	clock.Stop(len(exportList))
 	return nil
 }
 
@@ -66,17 +70,18 @@ func openTargetFile(in, action string, useLog *log.Logger) *os.File {
 		useLog.Fatalf("error opening file: %v", err)
 		panic(err)
 	}
-	useLog.Printf("%v=[%v] File=[%v]", action, in, dataFileHandle.Name())
+	useLog.Printf("%ving %vs from File=[%v]", action, in, dataFileHandle.Name())
 	return dataFileHandle
 }
 
 func ImportCSV[T any](importName string, entryTypeToInsert T, importProcessor func(*T) (string, error)) error {
 
+	clock := timing.Start(importName, actions.IMPORT.GetCode(), "")
 	// Create a slice of entryTypeToInsert to hold the data from the CSV file
 	insertEntriesList := []T{}
 
-	csvFile := openTargetFile(importName, importString, logHandler.ImportLogger)
-	defer csvFile.Close()
+	importFile := openTargetFile(importName, importString, logHandler.ImportLogger)
+	defer importFile.Close()
 
 	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader {
 		r := csv.NewReader(in)    // Allows use pipe as delimiter
@@ -86,14 +91,16 @@ func ImportCSV[T any](importName string, entryTypeToInsert T, importProcessor fu
 		return r                  // Allows use pipe as delimiter
 	})
 
-	if err := gocsv.UnmarshalFile(csvFile, &insertEntriesList); err != nil { // Load clients from file
+	if err := gocsv.UnmarshalFile(importFile, &insertEntriesList); err != nil { // Load clients from file
 		logHandler.ImportLogger.Printf("Importing %v: %v - No Content, nothing to import.", importName, err.Error())
-		csvFile.Close()
+		importFile.Close()
+		clock.Stop(0)
 		return nil
 	}
 
-	if _, err := csvFile.Seek(0, 0); err != nil { // Go to the start of the file
-		logHandler.ImportLogger.Printf("Importing %v: %v", importName, err.Error())
+	if _, err := importFile.Seek(0, 0); err != nil { // Go to the start of the file
+		logHandler.ImportLogger.Panicf("Importing %v: %v - Unable to fet to start of file.", importName, err.Error())
+		clock.Stop(0)
 		panic(err)
 	}
 
@@ -101,19 +108,21 @@ func ImportCSV[T any](importName string, entryTypeToInsert T, importProcessor fu
 
 	count := 0
 	for thisPos, insertEntry := range insertEntriesList {
-		logHandler.ImportLogger.Printf("Import %v (%v/%v)", importName, thisPos+1, totalImportEntries)
+		//logHandler.ImportLogger.Printf("Import %v (%v/%v)", importName, thisPos+1, totalImportEntries)
 		// the load function is a helper function to create a new entry instance and save it to the database
 		// the parameters should be customised to suit the specific requirements of the entryination table/DAO.
 		entryIdentifier, err := importProcessor(&insertEntry)
 		if err != nil {
-			logHandler.ImportLogger.Panicf("Error importing %v [%v] [%v]", importName, entryIdentifier, err.Error())
+			logHandler.ImportLogger.Panicf("Error importing %v [%v] Error=[%v]", importName, entryIdentifier, err.Error())
 			continue
 		}
 		count++
-		logHandler.ImportLogger.Printf("Import %v (%v/%v) - %v=[%v]", importName, thisPos, totalImportEntries, domain, entryIdentifier)
+		logHandler.ImportLogger.Printf("Importing %v (%v/%v) %v=[%v]", importName, thisPos+1, totalImportEntries, importName, entryIdentifier)
 	}
 
-	logHandler.ImportLogger.Printf("Imported (%v/%v) %v", count, totalImportEntries, importName)
-	csvFile.Close()
+	logHandler.ImportLogger.Printf("Imported (%v/%v) %v(s) from [%v]", count, totalImportEntries, importName, importFile.Name())
+	logHandler.EventLogger.Printf("Imported (%v/%v) %v(s) from [%v]", count, totalImportEntries, importName, importFile.Name())
+	importFile.Close()
+	clock.Stop(count)
 	return nil
 }
